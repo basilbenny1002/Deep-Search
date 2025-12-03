2ec5df11abe1ed525cf68e6f60584a221e85102e
basilbenny1002 <basilbenny1002@gmail.com>
2025-12-04 00:03:48 +0530

    Update app name on build.rs

--- build.rs
+++ build.rs
@@ -2,6 +2,9 @@ fn main() {
     if cfg!(target_os = "windows") {
         let mut res = winres::WindowsResource::new();
         res.set_icon("assets/deep_search.ico");
+        res.set("FileDescription", "Deep Search");
+        res.set("ProductName", "Deep Search");
+        res.set("OriginalFilename", "Deep Search.exe");
         res.set_manifest(r#"
 <assembly xmlns="urn:schemas-microsoft-com:asm.v1" manifestVersion="1.0">
 <trustInfo xmlns="urn:schemas-microsoft-com:asm.v3">
